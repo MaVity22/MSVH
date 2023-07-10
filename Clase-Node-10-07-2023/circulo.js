15b6f5922a48b77324ff7de077f37cf43990f610
MaVity22 <mateos.viteri@gmail.com>
2023-07-10 08:15:56 -0500

    Cambios 1

--- Clase-Node-10-07-2023/circulo.js
+++ Clase-Node-10-07-2023/circulo.js
@@ -1,5 +1,10 @@
 // Modulo 2: Fichero con librería circulo.js
 var PI = Math.PI;
+/*exports.valorPI = function () {
+    return PI;
+}*/
+// Exportar variables del moódulo 
+exports.valorPI = PI;
 
 exports.area = function (r) {
     return PI * r * r;
@@ -8,4 +13,4 @@ exports.circunferencia = function (r) {
     return 2 * PI * r;
 }
 
-//console.log(require);
+console.log(require);
