6ca6ffdd7fcf16c8b0a67dc8a07da0b897cceceb
MaVity22 <mateos.viteri@gmail.com>
2023-07-11 16:00:05 -0500

    Final

--- Clase-Node-10-07-2023/server.js
+++ Clase-Node-10-07-2023/server.js
@@ -1,10 +1,13 @@
 const express = require('express')
-const app = express()
-
 // Se exportan los usuarios
 const {listaUsuarios} = require('./data/data.js')
 // Puerto a utilizar
 const port = 8000;
+const app = express()
+// Middleware para que el servidor acepte las peticiones POST
+app.use(express.json());
+// Middleware para hacer el encode de la petición POST desde el URL
+app.use(express.urlencoded({extended: true}));
 
 app.get('/', function (req, res) {
   res.json({status: true, msg: 'Mateo Viteri'});
@@ -14,6 +17,32 @@ app.get('/home', function (req, res) {
 })
 
 app.get('/api/users',(req, res) => {
-  res.json(listaUsuarios)
+  res.json(listaUsuarios);
+})
+
+app.post('/api/user/new',(req, res) => {
+  listaUsuarios.push(req.body);
+  //console.log(typeof req.body);
+  res.json({status: true, msg: 'Usuario ingresado exitosamente'});
+})
+// Peticion GET para obtener un usuario por su ID
+app.get('/api/user/:id', (req, res) => {
+  //console.log(req.params);
+  res.json(listaUsuarios[req.params.id]);
+})
+
+// Peticion PUT para actualizar el usuario
+app.put('/api/user/:id', (req, res) => {
+  let id = req.params.id;
+  listaUsuarios[id] = req.body;
+  res.json({status: true, msg: 'Usuario actualizado'});
+})
+
+// Peticion DELETE para eliminar un usuario
+app.delete('/api/user/:id', (req, res) => {
+  let id = req.params.id;
+  listaUsuarios.splice(id,1);
+  //listaUsuarios[id] = req.body;
+  res.json({status: true, msg: 'Usuario eliminado'});
 })
 app.listen(port, () => console.log('Escuchando en puerto :',port));
